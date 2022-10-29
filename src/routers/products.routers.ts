d1b995e115763fd504ca9b762acf945d7232a4a2
Lucas Viana <lucasvfv@gmail.com>
2022-10-29 09:43:44 -0300

    feat: add errorHandler

--- src/routers/products.routers.ts
+++ src/routers/products.routers.ts
@@ -4,5 +4,11 @@ import { findProduct } from "../controllers/products.controllers.js";
 const productRouter = Router();
 
 productRouter.get('/products/:code', findProduct)
+productRouter.get('/products/', )
+productRouter.put('/products/:code')
+productRouter.delete('/products/:code')
+
+
+
 
 export default productRouter
